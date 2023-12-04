af68658bec219131a212ce3d2b261ddb715333c5
Bruno Risso <brunorisso58@gmail.com>
2023-12-04 17:08:59 -0300

    PIPA #23 - Setting image as longText

--- database/migrations/2014_10_11_132841_create_images_table.php
+++ database/migrations/2014_10_11_132841_create_images_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->text('image');
+            $table->longText('image');
             $table->softDeletes();
             $table->timestamps();
         });
