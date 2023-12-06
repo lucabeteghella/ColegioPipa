27956edf40255721144890f42f696a36960eac54
Bruno Risso <brunorisso58@gmail.com>
2023-12-06 16:23:45 -0300

    Setting post description as text instead of string

--- database/migrations/2023_11_08_231302_create_posts_table.php
+++ database/migrations/2023_11_08_231302_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->unsignedBigInteger('category_id');
             $table->unsignedBigInteger('tag_id');
             $table->unsignedBigInteger('image_id')->nullable();
