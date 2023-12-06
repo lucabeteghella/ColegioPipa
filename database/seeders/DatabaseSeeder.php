2fd411add71ceaf6bafec11a0192e8bad67f9b30
Bruno Risso <brunorisso58@gmail.com>
2023-12-06 15:38:52 -0300

    PIPA #30 - Creating users seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -30,5 +31,17 @@ class DatabaseSeeder extends Seeder
             ['permission'=> 'Comum'],
         ];
         DB::table('permissions')->insert($permissions);
+
+        $users = [
+            [
+                'name' => 'Bruno Risso',
+                'email' => 'brunorisso@email.com',
+                'phone_number' => '+55 19 99999-9999',
+                'cpf' => '12345678910',
+                'permission_id' => 1,
+                'password' => Hash::make('teste'),
+            ]
+        ];
+        DB::table('users')->insert($users);
     }
 }
